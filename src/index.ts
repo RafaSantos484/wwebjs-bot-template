29f79016f1295353a540b0b391165acc220aef93
rafael.santos <rafaelsantos32141@gmail.com>
2024-07-30 16:52:57 -0300

    First commit - template files

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Client, LocalAuth } from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("authenticated", () => {
+  console.log("Client is authenticated");
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("message_create", (message) => {
+  console.log(message);
+  if (message.body === "!ping") message.reply("Só o básico");
+});
+
+client.initialize();
